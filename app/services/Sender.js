b4796b7f5676d0a163f230290f12cb5edbe95a20
Tane Piper <piper.tane@gmail.com>
2014-03-28 12:21:27 +0000

    Corrected codec range

--- app/services/Sender.js
+++ app/services/Sender.js
@@ -3,8 +3,12 @@ angular.module('adon')
     '$rootScope',
     '$q',
     function($rootScope, $q) {
-      var adOnSoundLib = require('./lib/index.js')
-      var sender = new adOnSoundLib.Sender()
+      var adOnSoundLib = require('./lib/index.js');
+      var sender = new adOnSoundLib.Sender({
+        codec: new adOnSoundLib.Codec({
+          characters: 'abcdefghijklmnopqrstuvwxyz0123456789'
+        })
+      });
 
       return sender;
     }
